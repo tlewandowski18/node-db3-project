7852917e3bf2cda3f8737de5cabd9112cc4363e9
tlewandowski18 <tlewandowski18@gmail.com>
2020-04-29 07:33:17 -0400

    Complete two multi-table queries

--- queries.sql
+++ queries.sql
@@ -2,8 +2,16 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+SELECT c."CategoryName", p."ProductName" FROM "Category" AS c
+JOIN "Product" as p ON p."CategoryID" = c."Id";
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+
+SELECT p."ProductName", od."Quantity" FROM "OrderDetail" AS od
+JOIN "Product" as p ON p."Id" = od."ProductId"
+WHERE od."OrderId" = 10251
+ORDER BY p."ProductName";
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
